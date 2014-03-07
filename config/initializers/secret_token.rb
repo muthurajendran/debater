c427bdf99dc59f6bafec9ccb4318dfe0c743de90
Muthu <muthurajendranrg@gmail.com>
2014-03-07 15:14:45 +0530

    Intitial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Debater::Application.config.secret_key_base = '86fcfdcada361eb59e5bb182d84578fd04754ea94ccfd4fc2056846b0457a5fbea46fcadf261b1389899434f27e8e34277c67e77c9266861f2d19a2c41d4dbd4'
